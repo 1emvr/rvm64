09a694fb0e458ad894aa084ddf9dae4b3fd1d051
1emvr <lemur.proxymslns@outlook.com>
2025-05-16 06:45:24 -0400

    adding notes to decode for immediates

--- vmcs.cpp
+++ vmcs.cpp
@@ -35,6 +35,39 @@ __rdata const opcode encoding[] = {
     { 0b0100111, stype  }, { 0b1100011, btype  }, { 0b0010111, utype  }, { 0b0110111, utype  }, { 0b1101111, jtype  },
 };
 
+constexpr int32_t sign_extend(int32_t val, int bits) {
+    int shift = 32 - bits;
+    return (val << shift) >> shift;
+}
+
+inline int32_t imm_i(uint32_t opcode) {
+    return (int32_t)opcode >> 20;
+}
+
+inline int32_t imm_s(uint32_t opcode) {
+    return sign_extend(((opcode >> 25) << 5) | ((opcode >> 7) & 0x1F), 12);
+}
+
+inline int32_t imm_b(uint32_t opcode) {
+    int32_t val = (((opcode >> 31) & 1) << 12)
+                | (((opcode >> 25) & 0x3F) << 5)
+                | (((opcode >> 8) & 0xF) << 1)
+                | (((opcode >> 7) & 1) << 11);
+    return sign_extend(val, 13);
+}
+
+inline int32_t imm_u(uint32_t opcode) {
+    return opcode & 0xFFFFF000;
+}
+
+inline int32_t imm_j(uint32_t opcode) {
+    int32_t val = (((opcode >> 31) & 1) << 20)
+                | (((opcode >> 21) & 0x3FF) << 1)
+                | (((opcode >> 20) & 1) << 11)
+                | (((opcode >> 12) & 0xFF) << 12);
+    return sign_extend(val, 21);
+}
+
 __function uintptr_t vm_decode(uint32_t opcode) {
     uint8_t decoded = 0;
     uint8_t opcode7 = opcode & 0x7F;
@@ -61,23 +94,11 @@ __function uintptr_t vm_decode(uint32_t opcode) {
     uint8_t rs1   = (opcode >> 15) & 0x1F;
     uint8_t rd    = (opcode >> 7) & 0x1F;
 
-    int32_t imm_i = (int32_t)(opcode) >> 20;
-    int32_t imm_s = ((int32_t)(((opcode >> 25) << 5) | ((opcode >> 7) & 0x1F))) << 20 >> 20;
-    int32_t imm_u = opcode & 0xFFFFF000;
-
-    int32_t imm_b =
-    ((opcode >> 31) & 0x1) << 12 |
-    ((opcode >> 25) & 0x3F) << 5 |
-    ((opcode >> 8) & 0xF) << 1 |
-    ((opcode >> 7) & 0x1) << 11;
-    imm_b = (int32_t)(imm_b << 19) >> 19;  // Sign-extend from 13 bits
-
-    int32_t imm_j =
-    ((opcode >> 31) & 0x1) << 20 |
-    ((opcode >> 21) & 0x3FF) << 1 |
-    ((opcode >> 20) & 0x1) << 11 |
-    ((opcode >> 12) & 0xFF) << 12;
-    imm_j = (int32_t)(imm_j << 11) >> 11;  // Sign-extend from 21 bits
+    int32_t _imm_i = imm_i(opcode);
+    int32_t _imm_s = imm_s(opcode);
+    int32_t _imm_u = imm_u(opcode);
+    int32_t _imm_b = imm_b(opcode);
+    int32_t _imm_j = imm_j(opcode);
 
     vmcs->vscratch1[0] = func3;
     vmcs->vscratch1[1] = rd;
@@ -88,23 +109,23 @@ __function uintptr_t vm_decode(uint32_t opcode) {
     vmcs->vscratch1[6] = func5;
     vmcs->vscratch1[7] = func2;
 
-    vmcs->vscratch2[0] = imm_i;
-    vmcs->vscratch2[1] = imm_s;
-    vmcs->vscratch2[2] = imm_b;
-    vmcs->vscratch2[3] = imm_u;
-    vmcs->vscratch2[4] = imm_j;
+    vmcs->vscratch2[0] = _imm_i;
+    vmcs->vscratch2[1] = _imm_s;
+    vmcs->vscratch2[2] = _imm_b;
+    vmcs->vscratch2[3] = _imm_u;
+    vmcs->vscratch2[4] = _imm_j;
 
     switch(decoded) {
         // I_TYPE
         case itype: switch(opcode7) {
             case 0b0010011: switch(func3) {
-                case 0b000: { return ((uintptr_t*)vmcs->handler)[_addi];  } // TODO: drop fields into vmcs->vscratch
-                case 0b010: { return ((uintptr_t*)vmcs->handler)[_slti];  }
-                case 0b011: { return ((uintptr_t*)vmcs->handler)[_sltiu]; }
-                case 0b100: { return ((uintptr_t*)vmcs->handler)[_xori];  }
-                case 0b110: { return ((uintptr_t*)vmcs->handler)[_ori];   }
-                case 0b111: { return ((uintptr_t*)vmcs->handler)[_andi];  }
-                case 0b001: { return ((uintptr_t*)vmcs->handler)[_slli];  }
+                case 0b000: return ((uintptr_t*)vmcs->handler)[_addi];
+                case 0b010: return ((uintptr_t*)vmcs->handler)[_slti];
+                case 0b011: return ((uintptr_t*)vmcs->handler)[_sltiu];
+                case 0b100: return ((uintptr_t*)vmcs->handler)[_xori];
+                case 0b110: return ((uintptr_t*)vmcs->handler)[_ori];
+                case 0b111: return ((uintptr_t*)vmcs->handler)[_andi];
+                case 0b001: return ((uintptr_t*)vmcs->handler)[_slli];
                 case 0b101: switch(func7) {
                     case 0b0000000: return ((uintptr_t*)vmcs->handler)[_srli];
                     case 0b0100000: return ((uintptr_t*)vmcs->handler)[_srai];
