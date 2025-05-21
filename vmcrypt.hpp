106415564e8f68ba791236cbf6e8870d468620a3
1emvr <lemur.proxymslns@outlook.com>
2025-05-21 04:06:52 -0400

    adding notes for a hyperv architecture (still in planning phase). Go ahead with single-threaded for now.

--- vmcrypt.hpp
+++ vmcrypt.hpp
@@ -1,5 +1,6 @@
 #include "vmmain.hpp"
 namespace rvm64::crypt {
+    // NOTE: xor for now. rolling decryption won't come for a while.
     __inline __function uintptr_t decrypt_ptr(uintptr_t ptr) {
         return ptr ^ __key;
     }
