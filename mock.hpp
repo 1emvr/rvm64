d4f0451186fab5d579a385e0d8206937107d3e0b
Lemur <103511211+1emvr@users.noreply.github.com>
2025-05-22 06:48:54 -0400

    Update README.md

--- mock.hpp
+++ mock.hpp
@@ -9,7 +9,9 @@
 #include "vmelf.hpp"
 
 bool read_program_from_packet() {
+	BOOL success = false;
 	NTSTATUS status = 0;
+
 	HANDLE hfile = ctx->win32.NtCreateFile("./test.o", 
 			GENERIC_READ, FILE_SHARE_READ, NULL, 
 			OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
@@ -17,7 +19,7 @@ bool read_program_from_packet() {
 	if (hfile == INVALID_HANDLE_VALUE) {
 		vmcs->halt = 1;
 		vmcs->reason = status;
-		return false;
+		goto defer;
 	}
 
 	status = ctx->win32.NtGetFileSize(hfile, &vmcs->program.size);
@@ -25,7 +27,7 @@ bool read_program_from_packet() {
 	if (status == INVALID_FILE_SIZE || size == 0) {
 		vmcs->halt = 1;
 		vmcs->reason = status;
-		return false;
+		goto defer;
 	}
 
 	void* elf_data = nullptr;
@@ -35,24 +37,25 @@ bool read_program_from_packet() {
 
 		vmcs->halt = 1;
 		vmcs->reason = status;
-		return false;
+		goto defer;
 	}
 
 	DWORD bytesRead = 0;
 	if (!ctx->win32.NtReadFile(hfile, vmcs->program.address, vmcs->program.size, &bytesRead, NULL) || 
 			bytesRead != size) {
 
-		ctx->win32.NtFreeVirtualMemory(NtCurrentProcess(), (LPVOID)&vmcs->program.address, MEM_RELEASE);
 		vmcs->halt = 1;
 		vmcs->reason = vm_invalid_value;
-		return false;
+		goto defer;
 	}
 
 	bool success = rvm64::elf::load_elf64_image();
 	// TODO: patch .got/.plt
 
-	// Free the temporary ELF file buffer
-	ctx->win32.NtFreeVirtualMemory(NtCurrentProcess(), (LPVOID)&vmcs->program.address, &vmcs->program.size, MEM_RELEASE);
+defer:
+	if (vmcs->program.address) {
+		ctx->win32.NtFreeVirtualMemory(NtCurrentProcess(), (LPVOID)&vmcs->program.address, &vmcs->program.size, MEM_RELEASE);
+	}
 
 	vmcs->program.address = 0;
 	return success;
