72ae1226396be0e52b5625d808521da180b9ec3c
1emvr <lemur.proxymslns@outlook.com>
2025-06-30 01:05:42 -0400

    getting rid of the fake ctx thing

--- test.cpp
+++ test.cpp
@@ -1,10 +1,7 @@
-#include <cstring>
+#include <string.h>
+#include <stdlib.h>
 
-extern "C" void* malloc(size_t size);
-extern "C" void free(void *ptr);
-extern "C" void* memcpy(void* dst, void* src, size_t num);
-extern "C" int printf (const char * format, ...);
-
+// NOTE: do not mangle "main"
 extern "C" int main() {
 	int result = 0xFFFF;
 	void *buffer = malloc(sizeof(int));
