4d921197d47cb605235b4cc182f6b9ca2bf8d9a8
1emvr <lemur.proxymslns@outlook.com>
2025-07-09 04:36:59 -0400

    TODO: create and MSVC build for debugging

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,14 +2,7 @@
 set -e
 
 sudo apt update
-sudo apt install -y \
-  git \
-  clang \
-  lld \
-  make \
-  mingw-w64 \
-  cargo \
-  build-essential
+sudo apt install -y git clang lld make mingw-w64 cargo build-essential
 
 git clone https://github.com/kraj/musl.git
 cd musl
