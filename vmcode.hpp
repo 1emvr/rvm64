f6412f4d53fbff13f1e20184917f0ab206fd67b8
1emvr <lemur.proxymslns@outlook.com>
2025-07-15 01:38:20 -0400

    moving all data tables to .data since llvm-mingw wants to be picky

--- vmcode.hpp
+++ vmcode.hpp
@@ -56,7 +56,7 @@ namespace rvm64::decoder {
 		typenum type;
 	};
 
-	_rdata const opcode encoding[] = {
+	_data opcode encoding[] = {
 		{0b1010011, rtype}, {0b1000011, rtype}, {0b0110011, rtype}, {0b1000111, r4type}, {0b1001011, r4type}, {0b1001111, r4type},
 		{0b0000011, itype}, {0b0001111, itype}, {0b1100111, itype}, {0b0010011, itype}, {0b1110011, itype}, {0b0100011, stype},
 		{0b0100111, stype}, {0b1100011, btype}, {0b0010111, utype}, {0b0110111, utype}, {0b1101111, jtype},
