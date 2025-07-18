efc69e00e2c898dbaa656e0326c568ec20cca663
1emvr <lemur.proxymslns@outlook.com>
2025-07-18 12:02:41 -0400

    adding another opcode to the table that was missing from itype

--- vmcode.hpp
+++ vmcode.hpp
@@ -58,8 +58,8 @@ namespace rvm64::decoder {
 
 	_data opcode encoding[] = {
 		{0b1010011, rtype}, {0b1000011, rtype}, {0b0110011, rtype}, {0b1000111, r4type}, {0b1001011, r4type}, {0b1001111, r4type},
-		{0b0000011, itype}, {0b0001111, itype}, {0b1100111, itype}, {0b0010011, itype}, {0b1110011, itype}, {0b0100011, stype},
-		{0b0100111, stype}, {0b1100011, btype}, {0b0010111, utype}, {0b0110111, utype}, {0b1101111, jtype},
+		{0b0000011, itype}, {0b0001111, itype}, {0b1100111, itype}, {0b0010011, itype}, {0b1110011, itype}, {0b0011011, itype},
+		{0b0100011, stype}, {0b0100111, stype}, {0b1100011, btype}, {0b0010111, utype}, {0b0110111, utype}, {0b1101111, jtype},
 	};
 
 	_vmcall void vm_decode(uint32_t opcode) {
