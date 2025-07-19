5a6baf9da970638082145a7d5746bdb6b1ab9eab
1emvr <lemur.proxymslns@outlook.com>
2025-07-19 08:41:52 -0400

    *@plt seems to do nothing - skips through the decode section

--- test.cpp
+++ test.cpp
@@ -1,13 +1,19 @@
 #include <string.h>
 #include <stdlib.h>
+#define ebreak()  asm volatile("ebreak")
 
 // NOTE: do not mangle "main"
 extern "C" int main() {
 	constexpr int result = 0xFFFF;
+
+	ebreak();
 	void *buffer = malloc(sizeof(int));
 
+	ebreak();
 	memcpy(buffer, &result, sizeof(int));
+	ebreak();
 	memset(buffer, 0, sizeof(int));
+	ebreak();
 	free(buffer);
 
 	return 0;
