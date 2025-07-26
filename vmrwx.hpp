23c80792858a63efe5211cf8e87e8607226e2823
1emvr <lemur.proxymslns@outlook.com>
2025-07-26 00:23:41 -0400

    added mmu checks to mem read/write macros. needs tested

--- vmrwx.hpp
+++ vmrwx.hpp
@@ -18,41 +18,41 @@
 	 (addr) < (uintptr_t)(vmcs->vstack + VSTACK_MAX_CAPACITY))
 
 
-#define mem_read_check(T, addr)  												\
-	do {                                       									\
-		auto m = rvm64::mmu::memory_check(addr);  								\
-		if (m) { 																\
-			addr = m; 															\
-			break; 																\
-		} 																		\
-		if ((addr) % sizeof(T) != 0) {                                         	\
-			CSR_SET_TRAP(vmcs->pc, load_address_misaligned, 0, addr, 1);       	\
-		}                                                                      	\
-		if (!STACK_MEMORY_IN_BOUNDS(addr) && !PROCESS_MEMORY_IN_BOUNDS(addr)) {	\
-			CSR_SET_TRAP(vmcs->pc, load_access_fault, 0, addr, 1);      		\
-		} 																		\
+#define mem_read_check(T, addr)  													\
+	do {                                       										\
+		auto m = rvm64::mmu::memory_check((void*)addr);  							\
+		if (m) { 																	\
+			addr = m; 																\
+			break; 																	\
+		} 																			\
+		if ((addr) % sizeof(T) != 0) {                                         		\
+			CSR_SET_TRAP(vmcs->pc, load_address_misaligned, 0, addr, 1);       		\
+		}                                                                      		\
+		if (!(STACK_MEMORY_IN_BOUNDS(addr)) && !(PROCESS_MEMORY_IN_BOUNDS(addr))) {	\
+			CSR_SET_TRAP(vmcs->pc, load_access_fault, 0, addr, 1);      			\
+		} 																			\
 	} while (0)
 
 
-#define mem_write_check(T, addr) 												\
-	do {                                      									\
-		auto m = rvm64::mmu::memory_check(addr);  								\
-		if (m) { 																\
-			addr = m; 															\
-			break; 																\
-		} 																		\
-		if ((addr) % sizeof(T) != 0) {                                         	\
-			CSR_SET_TRAP(vmcs->pc, store_amo_address_misaligned, 0, addr, 1);  	\
-		}                                                                      	\
-		if (!STACK_MEMORY_IN_BOUNDS(addr) && !PROCESS_MEMORY_IN_BOUNDS(addr)) {	\
-			CSR_SET_TRAP(vmcs->pc, store_amo_access_fault, 0, addr, 1);			\
-		} 																		\
+#define mem_write_check(T, addr) 													\
+	do {                                      										\
+		auto m = rvm64::mmu::memory_check((void*)addr);  							\
+		if (m) { 																	\
+			addr = m; 																\
+			break; 																	\
+		} 																			\
+		if ((addr) % sizeof(T) != 0) {                                         		\
+			CSR_SET_TRAP(vmcs->pc, store_amo_address_misaligned, 0, addr, 1);  		\
+		}                                                                      		\
+		if (!(STACK_MEMORY_IN_BOUNDS(addr)) && !(PROCESS_MEMORY_IN_BOUNDS(addr))) {	\
+			CSR_SET_TRAP(vmcs->pc, store_amo_access_fault, 0, addr, 1);				\
+		} 																			\
 	} while (0)
 
 
 #define reg_read(T, dst, reg_idx) 	dst = (T)vmcs->vregs[(reg_idx)]
 #define scr_read(T, dst, scr_idx) 	dst = (T)vmcs->vscratch[(scr_idx)]
-#define mem_read(T, retval, addr)  	mem_read_check((T), (addr)); retval = *(T *)((uintptr_t)addr);
+#define mem_read(T, retval, addr)  	mem_read_check((T), (addr)); retval = *(T *)(addr);
 
 #define reg_write(T, reg_idx, src) 	if (reg_idx != 0) vmcs->vregs[(reg_idx)] = (T)(src);
 #define scr_write(T, scr_idx, src) 	if (scr_idx <= imm) vmcs->vscratch[(scr_idx)] = (T)(src);
