c6fea7131de72df9ff2ce9c6aa8d83dcba76fc04
1emvr <lemur.proxymslns@outlook.com>
2025-07-30 03:12:57 -0400

    buffer check does not pass

--- vmmu.hpp
+++ vmmu.hpp
@@ -53,7 +53,7 @@ namespace rvm64::mmu {
 
 	_native uint8_t* memory_check(uintptr_t guest) {
 		if (guest == 0) {
-			return 0;
+			return nullptr;
 		}
 		for (const auto& entry : native_exec_regions) {
 			if (guest >= entry.guest_addr && guest < entry.guest_addr + entry.length) {
@@ -61,7 +61,7 @@ namespace rvm64::mmu {
 				return (uint8_t*) entry.host_addr + offset; // risc-v usable address for calculating non-zero offsets (host[n + i])
 			}
 		}
-		return 0;
+		return nullptr;
 	}
 
 #define PROT_READ	0x1		
