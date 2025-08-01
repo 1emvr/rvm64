b0bec914a8988d93ee3caefca218bfdb3f9fbdc7
1emvr <lemur.proxymslns@outlook.com>
2025-08-01 11:40:11 -0400

    definitely skipping an instruction when returning from VEH??

--- vmmain.hpp
+++ vmmain.hpp
@@ -6,11 +6,6 @@
 
 #include "vmcommon.hpp"
 
-typedef struct {
-	uintptr_t start;
-	uintptr_t end;
-} vm_range;
-
 
 typedef struct {
 	uint8_t *address;
@@ -18,13 +13,23 @@ typedef struct {
 	uintptr_t stat;
 } vm_buffer_t;
 
-
 struct intel_t {
     uint64_t rip, rsp, rax, rbx, rcx, rdx, rsi, rdi, rbp;
     uint64_t r8, r9, r10, r11, r12, r13, r14, r15;
     uint64_t rflags;
 };
 
+struct {
+	uint8_t *address;
+	size_t size;
+} vm_process;
+
+struct {
+	uintptr_t m_epc;
+	uintptr_t m_cause;
+	uintptr_t m_status;
+	uintptr_t m_tval;
+} vm_csr;
 
 typedef struct {
 	uintptr_t pc;
@@ -38,17 +43,8 @@ typedef struct {
 	jmp_buf trap_handler;
 	jmp_buf exit_handler;
 
-	struct {
-		uint8_t *address;
-		size_t size;
-	} process;
-
-	struct {
-		uintptr_t m_epc;
-		uintptr_t m_cause;
-		uintptr_t m_status;
-		uintptr_t m_tval;
-	} csr;
+	vm_process process;
+	vm_csr csr;
 
 	int trap;
 	int cache;
