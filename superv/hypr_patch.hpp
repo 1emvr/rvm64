0e86e1f086aa4af8d60cc9e496f3c4d000943afd
1emvr <lemur.proxymslns@outlook.com>
2025-08-04 04:32:29 -0400

    added shitty print debugger

--- superv/hypr_patch.hpp
+++ superv/hypr_patch.hpp
@@ -1,25 +1,30 @@
 #ifndef HYPRPATCH_HPP
 #define HYPRPATCH_HPP
 #include <windows.h>
+
 #include "hypr_proc.hpp"
+#include "../vmmain.hpp"
+
+_data uint8_t entry_sig[] = {
+	0x48, 0x89, 0x05, 0x01, 0x3f, 0x01, 0x00, 	// 0x00: mov     cs:vmcs, rax
+	0xe8, 0x3d, 0xfe, 0xff, 0xff,             	// 0x07: call    rvm64::entry::vm_entry(void)
+	0x48, 0x8b, 0x05, 0xf5, 0x3e, 0x01, 0x00,  	// 0x0c: mov     rax, cs:vmcs
+};
+
+_data uint8_t hook_stub[] = {
+	0x8b, 0x05, 0x00, 0x00, 0x00, 0x00,        	// 0x00: mov eax, [rip+disp32]
+	0x85, 0xc0,                    				// 0x06: test eax, eax
+	0x75, 0xf8,                    				// 0x08: jne spin (-8)
+	0xc7, 0x05, 0x00, 0x00, 0x00, 0x00,			// 0x0a: mov dword ptr [rip+disp32], 0
+	0x00, 0x00, 0x00, 0x00,        				// 0x10: imm32 (zero)
+	0xe9, 0x00, 0x00, 0x00, 0x00,   			// 0x14: jmp rel32 to original vm_entry
+};
 
 namespace superv::patch {
-	bool install_entry_patch(process_t *proc, uintptr_t ready_addr) {
-		uint8_t entry_sig[] = {
-			0x48, 0x89, 0x05, 0x01, 0x3f, 0x01, 0x00, 	// 0x00: mov     cs:vmcs, rax
-			0xe8, 0x3d, 0xfe, 0xff, 0xff,             	// 0x07: call    rvm64::entry::vm_entry(void)
-			0x48, 0x8b, 0x05, 0xf5, 0x3e, 0x01, 0x00,  	// 0x0c: mov     rax, cs:vmcs
-		};
-		uint8_t hook_stub[] = {
-			0x8b, 0x05, 0x00, 0x00, 0x00, 0x00,        	// 0x00: mov eax, [rip+disp32]
-			0x85, 0xc0,                    				// 0x06: test eax, eax
-			0x75, 0xf8,                    				// 0x08: jne spin (-8)
-			0xc7, 0x05, 0x00, 0x00, 0x00, 0x00,			// 0x0a: mov dword ptr [rip+disp32], 0
-			0x00, 0x00, 0x00, 0x00,        				// 0x10: imm32 (zero)
-			0xe9, 0x00, 0x00, 0x00, 0x00,   			// 0x14: jmp rel32 to original vm_entry
-		};
 
+	bool install_entry_patch(process_t *proc, uintptr_t ready_addr) {
 		size_t stub_size = sizeof(hook_stub);
+
 		uintptr_t hook_addr = (uintptr_t)VirtualAllocEx(proc->handle, nullptr, stub_size, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
 		if (!hook_addr) {
 			return false;
@@ -62,19 +67,21 @@ namespace superv::patch {
 		return true;
 	}
 
+	_data uint8_t decoder_sig[] = { 
+		0x8B, 0x45, 0xFC,							// 0x00: mov     eax, [rbp+opcode]
+		0x89, 0xC1,                                 // 0x03: mov     ecx, eax        ; opcode
+		0xE8, 0x85, 0x07, 0xFF, 0xFF,               // 0x05: call    rvm64::decoder::vm_decode(uint)
+		0x48, 0x8B, 0x05, 0x75, 0x3F, 0x01, 0x00,   // 0x0a: mov     rax, cs:vmcs
+	};
+
+	_data uint8_t hook_stub[] = {
+		0x50,                                     // push rax
+		0xE8, 0x00, 0x00, 0x00, 0x00,             // call rel32 -> debugger_decode
+		0x58,                                     // pop rax
+		0xE9, 0x00, 0x00, 0x00, 0x00              // jmp rel32 -> vm_decode
+	};
+
 	bool install_step_patch() {
-		uint8_t decoder_sig[] = { 
-			0x8B, 0x45, 0xFC,							// 0x00: mov     eax, [rbp+opcode]
-			0x89, 0xC1,                                 // 0x03: mov     ecx, eax        ; opcode
-			0xE8, 0x85, 0x07, 0xFF, 0xFF,               // 0x05: call    rvm64::decoder::vm_decode(uint)
-			0x48, 0x8B, 0x05, 0x75, 0x3F, 0x01, 0x00,   // 0x0a: mov     rax, cs:vmcs
-		};
-		uint8_t hook_stub[] = {
-			0x50,                                     // push rax
-			0xE8, 0x00, 0x00, 0x00, 0x00,             // call rel32 -> debugger_decode
-			0x58,                                     // pop rax
-			0xE9, 0x00, 0x00, 0x00, 0x00              // jmp rel32 -> vm_decode
-		};
 	}
 }
 #endif // HYPRPATCH_HPP
