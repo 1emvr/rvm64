06bf494125d3e988a53f056d014a0860ec612335
1emvr <lemur.proxymslns@outlook.com>
2025-08-04 04:32:29 -0400

    added shitty print debugger

--- superv/hypr_load.hpp
+++ superv/hypr_load.hpp
@@ -2,6 +2,7 @@
 #define HYPRLOAD_HPP
 #include <windows.h>
 
+// TODO: separate scopes and create shmem/ipc namespace
 namespace superv::loader {
 	typedef struct {
 		HANDLE 			map;
@@ -9,11 +10,11 @@ namespace superv::loader {
 		PBYTE 			buffer;
 		SIZE_T 			size;
 		volatile int 	ready;
-	} shared_buffer;
+	} mapped_view;
 
 
-	shared_buffer* create_shared_buffer() {
-		HANDLE hmap = CreateFileMappingW(INVALID_HANDLE_VALUE, nullptr, PAGE_READWRITE, 0, sizeof(shared_buffer) + 0x100000, SHMEM_NAME);
+	mapped_view* create_mapped_view() {
+		HANDLE hmap = CreateFileMappingW(INVALID_HANDLE_VALUE, nullptr, PAGE_READWRITE, 0, sizeof(mapped_view) + 0x100000, SHMEM_NAME);
 		if (!hmap) {
 			printf("[-] CreateFileMappingW failed: %lu\n", GetLastError());
 			return nullptr;
@@ -26,13 +27,13 @@ namespace superv::loader {
 			return nullptr;
 		}
 
-		auto shbuf = (shared_buffer*)HeapAlloc(GetProcessHeap(), 0, sizeof(shared_buffer));
+		auto shbuf = (mapped_view*)HeapAlloc(GetProcessHeap(), 0, sizeof(mapped_view));
 		shbuf->map = hmap;
 		shbuf->view = view;
 		return shbuf;
 	}
 
-	void destroy_shared_buffer(shared_buffer** shbuf) {
+	void destroy_mapped_view(mapped_view** shbuf) {
 		if (*shbuf) {
 			if ((*shbuf)->view) {
 				UnmapViewOfFile((*shbuf)->view);
@@ -45,7 +46,8 @@ namespace superv::loader {
 		}
 	}
 
-	bool write_shared_buffer(shared_buffer* shbuf, const char* filepath) {
+	// TODO: change this and separeate mapped view RW from file loading
+	bool write_mapped_view(mapped_view* shbuf, const char* filepath) {
 		FILE* f = fopen(filepath, "rb");
 		if (!f) {
 			printf("[-] Failed to open file: %s\n", filepath);
@@ -61,11 +63,11 @@ namespace superv::loader {
 		if (fsize > 0x100000) {
 			printf("[-] ELF too large\n");
 			fclose(f);
-			destroy_shared_buffer(&shbuf);
+			destroy_mapped_view(&shbuf);
 			return false;
 		}
 
-		uint8_t* data = (uint8_t*)(shbuf->view + sizeof(shared_buffer)); // right after the shared_buffer struct
+		uint8_t* data = (uint8_t*)(shbuf->view + sizeof(mapped_view)); // right after the mapped_view struct
 		fread(data, 1, fsize, f);
 		fclose(f);
 
