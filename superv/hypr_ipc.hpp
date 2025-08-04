7112114de3d6a888e882a93dd7c2226c632e7da0
1emvr <lemur.proxymslns@outlook.com>
2025-08-04 04:32:29 -0400

    added shitty print debugger

--- superv/hypr_ipc.hpp
+++ superv/hypr_ipc.hpp
@@ -1,9 +1,13 @@
 #ifndef HYPRIPC_HPP
 #define HYPRIPC_HPP
+#include "../vmmain.hpp"
+
+#define MAPPED_VIEW_HEADER_SIZE sizeof(mapped_view)
+#define MAPPED_VIEW_MAX_MEMORY 0x100000
 
 namespace ipc {
 	mapped_view* create_mapped_view() {
-		HANDLE hmap = CreateFileMappingW(INVALID_HANDLE_VALUE, nullptr, PAGE_READWRITE, 0, sizeof(mapped_view) + 0x100000, SHMEM_NAME);
+		HANDLE hmap = CreateFileMappingW(INVALID_HANDLE_VALUE, nullptr, PAGE_READWRITE, 0, MAPPED_VIEW_HEADER_SIZE + MAPPED_VIEW_MAX_MEMORY, SHMEM_NAME); 
 		if (!hmap) {
 			printf("[-] CreateFileMappingW failed: %lu\n", GetLastError());
 			return nullptr;
@@ -22,14 +26,6 @@ namespace ipc {
 		return shbuf;
 	}
 
-	void read_mapped_view(mapped_view* shbuf) {
-
-	}
-
-	void write_mapped_view(mapped_view* shbuf) {
-
-	}
-
 	void destroy_mapped_view(mapped_view** shbuf) {
 		if (*shbuf) {
 			if ((*shbuf)->view) {
@@ -41,6 +37,30 @@ namespace ipc {
 			HeapFree(GetProcessHeap(), 0, (*shbuf));
 			*shbuf = nullptr;
 		}
+	}
+
+	bool read_mapped_view(mapped_view* shbuf, uint8_t* data, uintptr_t offset, size_t size) {
+		if (!shbuf || !data) {
+			return false;
+		}
+		if (offset > MAPPED_VIEW_MAX_MEMORY || size > MAPPED_VIEW_MAX_MEMORY - offset) {
+			return false;
+		}
+
+		memcpy(data, (uint8_t*)(shbuf->view + offset), size);
+		return true;
+	}
+
+	bool write_mapped_view(mapped_view* shbuf, const uint8_t* data, uintptr_t offset, size_t size) {
+		if (!shbuf || !data) {
+			return false;
+		}
+		if (offset > MAPPED_VIEW_MAX_MEMORY || size > MAPPED_VIEW_MAX_MEMORY - offset) {
+			return false;
+		}
+
+		memcpy((uint8_t*)(shbuf->view + offset), data, size);
+		return true;
 	}
 }
 #endif // HYPRIPC_HPP
