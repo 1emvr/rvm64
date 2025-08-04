115f7c4bf2721967be78ee7b089ed3fd0f8fca1b
1emvr <lemur.proxymslns@outlook.com>
2025-08-04 04:32:29 -0400

    added shitty print debugger

--- superv/hypr.cpp
+++ superv/hypr.cpp
@@ -202,28 +202,24 @@ namespace superv {
 		uintptr_t trampoline;
 	} patch_t;
 
-	uint8_t entry_sig[] = {
-		0x48, 0x89, 0x05, 0x01, 0x3f, 0x01, 0x00, 	// 0x00: mov     cs:vmcs, rax
-		0xe8, 0x3d, 0xfe, 0xff, 0xff,             	// 0x07: call    rvm64::entry::vm_entry(void)
-		0x48, 0x8b, 0x05, 0xf5, 0x3e, 0x01, 0x00  	// 0x0c: mov     rax, cs:vmcs
-	};
-	uint8_t hook_stub[] = {
-		0x8B, 0x05, 0x00, 0x00, 0x00, 0x00,        	// 0x00: mov eax, [rip+disp32]
-		0x85, 0xC0,                    				// 0x06: test eax, eax
-		0x75, 0xF8,                    				// 0x08: jne spin (-8)
-		0xC7, 0x05, 0x00, 0x00, 0x00, 0x00,			// 0x0A: mov dword ptr [rip+disp32], 0
-		0x00, 0x00, 0x00, 0x00,        				// 0x10: imm32 (zero)
-		0xE9, 0x00, 0x00, 0x00, 0x00   				// 0x14: jmp rel32 to original vm_entry
-	};
+	patch_t* install_entry_patch(process_t *proc, uintptr_t read_addr) {
+		uint8_t entry_sig[] = {
+			0x48, 0x89, 0x05, 0x01, 0x3f, 0x01, 0x00, 	// 0x00: mov     cs:vmcs, rax
+			0xe8, 0x3d, 0xfe, 0xff, 0xff,             	// 0x07: call    rvm64::entry::vm_entry(void)
+			0x48, 0x8b, 0x05, 0xf5, 0x3e, 0x01, 0x00,  	// 0x0c: mov     rax, cs:vmcs
+		};
+		uint8_t hook_stub[] = {
+			0x8b, 0x05, 0x00, 0x00, 0x00, 0x00,        	// 0x00: mov eax, [rip+disp32]
+			0x85, 0xc0,                    				// 0x06: test eax, eax
+			0x75, 0xf8,                    				// 0x08: jne spin (-8)
+			0xc7, 0x05, 0x00, 0x00, 0x00, 0x00,			// 0x0a: mov dword ptr [rip+disp32], 0
+			0x00, 0x00, 0x00, 0x00,        				// 0x10: imm32 (zero)
+			0xe9, 0x00, 0x00, 0x00, 0x00,   			// 0x14: jmp rel32 to original vm_entry
+		};
 
-	patch_t* install_entry_patch(process_t *proc) {
-		uintptr_t hook_addr = (uintptr_t)VirtualAllocEx(proc->handle, nullptr, sizeof(hook_stub), MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+		size_t stub_size = sizeof(hook_stub);
+		uintptr_t hook_addr = (uintptr_t)VirtualAllocEx(proc->handle, nullptr, stub_size, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
 		if (!hook_addr) {
-			return nullptr;
-		}
-
-		memset(hook_addr, 0, sizeof(hook_stub));
-		if (!superv::process::memory::write_proc_memory(proc->handle, hook_addr, hook_stub, sizeof(hook_stub))) {
 			return nullptr;
 		}
 
@@ -233,11 +229,27 @@ namespace superv {
 		}
 
 		int32_t original_rel = 0;
-		if (!superv::process::memory::read_proc_memory(proc->handle, entry_offset + 8 + 1, (const uint8_t*)&original_rel, sizeof(original_rel))) {
+		if (!superv::process::memory::read_proc_memory(proc->handle, entry_offset + 7 + 1, (uint8_t*)&original_rel, sizeof(original_rel))) {
 			return nullptr;
 		}
 
-		uintptr_t original_entry = entry_offset + 8 + 5 + original_rel;
+		uintptr_t original_entry = entry_offset + 7 + 5 + original_rel;
+		{
+			int32_t rel1 = (int32_t)(ready_addr - (hook_addr + 0x06)); // shmem->ready - (&hook + (i + &next_ins))
+			memcpy(&hook_stub[0x02], &rel1, sizeof(rel1));
+
+			int32_t rel2 = (int32_t)(ready_addr - (hook_addr + 0x10));
+			memcpy(&hook_stub[0x0c], &rel2, sizeof(rel2));
+
+			int32_t rel3 = (int32_t)(original_entry - (hook_addr + 0x19));
+			memcpy(&hook_stub[0x15], &rel3, sizeof(rel3));
+		}
+
+		if (!superv::process::memory::write_proc_memory(proc->handle, hook_addr, hook_stub, sizeof(hook_stub))) {
+			return nullptr;
+		}
+
+
 		int32_t hook_offset = (int32_t)(hook_addr - (entry_offset + 8 + 5));
 
 		if (!superv::process::memory::write_proc_memory(proc->handle, entry_offset + 8 + 1, (uint8_t*)&hook_offset, sizeof(hook_offset))) {
