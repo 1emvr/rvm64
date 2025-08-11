9c342abfbed0bcf686dab188364e48bac2a62745
1emvr <lemur.proxymslns@outlook.com>
2025-08-11 03:40:57 -0400

    changing model for the vm/superv ipc

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <setjmp.h>
 
 #include "vmmain.hpp"
+#include "vmentry.hpp"
 #include "vmmem.hpp"
 #include "vmcode.hpp"
 #include "vmcommon.hpp"
@@ -9,42 +10,6 @@
 #include "rvni.hpp"
 
 
-namespace rvm64::entry {
-	_vmcall void vm_exit() {
-		RemoveVectoredExceptionHandler(veh_handle);
-		rvm64::memory::memory_end();
-	}
-
-	_vmcall void vm_init(uint8_t *packet_data, size_t packet_size) {
-		veh_handle = AddVectoredExceptionHandler(1, vm_exception_handler);
-
-		rvm64::memory::memory_init(packet_size + VM_PROCESS_PADDING); // init vmcs->process.address 
-		rvm64::elf::load_elf_image(packet_data, packet_size);
-
-		rvm64::elf::patch_elf_plt(vmcs->process.address);
-		vmcs->vregs[sp] = (uintptr_t)(vmcs->vstack + VSTACK_MAX_CAPACITY);
-
-		vmcs->cache
-			? rvm64::memory::cache_data(packet_data, packet_size)
-			: rvm64::memory::destroy_data(packet_data, packet_size);
-	}
-
-	_vmcall void vm_entry() {
-		if (setjmp(vmcs->trap_handler)) {}
-
-		while (true) {
-			int32_t opcode = *(int32_t*)vmcs->pc;
-
-			if (opcode == RV64_RET) {
-				if (!PROCESS_MEMORY_IN_BOUNDS(vmcs->vregs[regenum::ra])) {
-					CSR_SET_TRAP(nullptr, environment_exit, 0, 0, 1);
-				}
-			}
-			rvm64::decoder::vm_decode(opcode); // decoder patch here -> hook for every new ins.
-			vmcs->pc += 4;
-		}
-	}
-};
 
 namespace rvm64 {
 	_native int32_t vm_main(MV *packet) {
