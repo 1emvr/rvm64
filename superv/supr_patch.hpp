e40fb421768a6803b952814ed239a451176da81a
1emvr <lemur.proxymslns@outlook.com>
2025-08-11 03:40:57 -0400

    changing model for the vm/superv ipc

--- superv/supr_patch.hpp
+++ superv/supr_patch.hpp
@@ -15,25 +15,25 @@ namespace superv::patch {
 		0xff,0xe0 
 	};	
 
-	bool setup_trampoline(HANDLE hprocess, uintptr_t callee, size_t n_prolg, uintptr_t* tramp_out) {
+	bool install_trampoline(HANDLE hprocess, uintptr_t callee, size_t n_prolg, uintptr_t* tramp_out) {
 		bool success = false;
 		void *trampoline = nullptr;
 		uint8_t *buffer = nullptr;
 		uint64_t back_addr = 0;
 
 		if (n_prolg < 12) {
-			printf("[ERR] setup_trampoline: function prologue must be at least 12 bytes\n"); 
+			printf("[ERR] install_trampoline: function prologue must be at least 12 bytes\n"); 
 			goto defer;
 		}
 
 		buffer = (uint8_t*)VirtualAlloc(nullptr, n_prolg + sizeof(jmp_back), MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
 		if (!buffer) {
-			printf("[ERR] setup_trampoline could not allocate it's own buffer: 0x%lx.\n", GetLastError()); 
+			printf("[ERR] install_trampoline could not allocate it's own buffer: 0x%lx.\n", GetLastError()); 
 			return goto defer;
 		}
 
 		if (!rvm64::memory::read_process_memory(hprocess, callee, buffer, n_prolg)) {
-			printf("[ERR] setup_trampoline could not read from the remote process: 0x%lx.\n", GetLastError()); 
+			printf("[ERR] install_trampoline could not read from the remote process: 0x%lx.\n", GetLastError()); 
 			return goto defer;
 		}
 
@@ -43,12 +43,12 @@ namespace superv::patch {
 		memcpy(buffer + n_prolg + 2, &back_addr, sizeof(uint64_t));
 
 		if (!(trampoline = VirtualAllocEx(hprocess, nullptr, n_prolg + sizeof(jmp_back), MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE))) {
-			printf("[ERR] setup_trampoline could not allocate in the remote process: 0x%lx.\n", GetLastError()); 
+			printf("[ERR] install_trampoline could not allocate in the remote process: 0x%lx.\n", GetLastError()); 
 			goto defer;
 		}
 
 		if (!rvm64::memory::write_process_memory(hprocess, (uintptr_t)trampoline, buffer, n_prolg)) {
-			printf("[ERR] setup_trampoline could not write to the remote process: 0x%lx.\n", GetLastError()); 
+			printf("[ERR] install_trampoline could not write to the remote process: 0x%lx.\n", GetLastError()); 
 			goto defer;
 		}
 
@@ -69,7 +69,7 @@ defer:
 		DWORD old_prot = 0;	
 
 		if (n_prolg < 12) {
-			printf("[ERR] setup_trampoline: function prologue must be at least 12 bytes\n"); 
+			printf("[ERR] install_trampoline: function prologue must be at least 12 bytes\n"); 
 			goto defer;
 		}
 
@@ -121,7 +121,7 @@ defer:
 	};
 
 	static constexpr size_t EH_OFF_SIGNAL_LOAD_DISP = 0x04; // next IP = hook+0x08
-	static constexpr size_t EH_OFF_SIGNAL_CLR_DISP  = 0x0F; // next IP = hook+0x13
+	static constexpr size_t EH_OFF_SIGNAL_CLR_DISP  = 0x0e; // next IP = hook+0x13
 	static constexpr size_t EH_OFF_RESUME_IMM64     = 0x15; // imm64 at +0x15
 														  
 	bool install_entry_hook(win_process *proc, vm_channel* channel) {
@@ -142,11 +142,11 @@ defer:
 		}
 
 		uintptr_t callee = sig_offset + 5 + original_rel;
-		size_t n_prolg = 12; // I'm not doing automated disasm to find the prologue size. It will be static and I'll have to change it accordingly.
 		uintptr_t trampoline = 0;
+		size_t n_prolg = 12; // I'm not doing automated disasm to find the prologue size. It will be static and I'll have to change it accordingly.
 		
-		if (!setup_trampoline(proc->handle, callee, n_prolg, &trampoline)) {
-			printf("[ERR] install_entry_hook::setup_trampoline failed: 0x%lx.\n", GetLastError()); 
+		if (!install_trampoline(proc->handle, callee, n_prolg, &trampoline)) {
+			printf("[ERR] install_entry_hook::install_trampoline failed: 0x%lx.\n", GetLastError()); 
 			return false;
 		}
 
@@ -162,8 +162,8 @@ defer:
 		x_memcpy(buffer, entry_hook, sizeof(entry_hook));
 		uintptr_t ch_signal = (uintptr_t)channel->self + offsetof(vm_channel, ipc.signal);
 		{
-			int32_t d32_sgn_load = (int32_t)(ch_signal - (hook_addr + 0x08)); 
-			int32_t d32_sgn_clear = (int32_t)(ch_signal - (hook_addr + 0x13)); 
+			int32_t d32_sgn_load = (int32_t)(ch_signal - (hook + 0x08)); 
+			int32_t d32_sgn_clear = (int32_t)(ch_signal - (hook + 0x13)); 
 
 			x_memcpy(&buffer[EH_OFF_SIGNAL_LOAD_DISP], 	&d32_sgn_load, 	sizeof(int32_t));
 			x_memcpy(&buffer[EH_OFF_SIGNAL_CLR_DISP], 	&d32_sgn_clear, sizeof(int32_t));
@@ -194,39 +194,29 @@ defer:
 
 	_rdata static const uint8_t decoder_hook[] = {
 		0xcc,                                   					// +0x00: int3
-		0x4c, 0x8d, 0x1d, 0x00, 0x00, 0x00, 0x00,                	// +0x01: lea r11, [rip+disp32]
-		0x41, 0x89, 0x03,                         					// +0x08: mov dword ptr [r11], eax
-		0x4c, 0x8d, 0x1d, 0x00, 0x00, 0x00, 0x00,                 	// +0x0b: lea r11, [rip+disp32]
-		0x49, 0x89, 0x0b,                         					// +0x12: mov qword ptr [r11], rcx
-		0x4c, 0x8d, 0x1d, 0x00, 0x00, 0x00, 0x00,                	// +0x15: lea r11, [rip+disp32]
+		0x4c, 0x8d, 0x1d, 0x00, 0x00, 0x00, 0x00,                	// +0x01: lea r11, [rip+disp32] (opcode)
+		0x41, 0x89, 0x03,                         					// +0x08: mov dword ptr [r11], eax 
+		0x4c, 0x8d, 0x1d, 0x00, 0x00, 0x00, 0x00,                 	// +0x0b: lea r11, [rip+disp32] (vmcs)
+		0x49, 0x89, 0x0b,                         					// +0x12: mov qword ptr [r11], rcx 
+		0x4c, 0x8d, 0x1d, 0x00, 0x00, 0x00, 0x00,                	// +0x15: lea r11, [rip+disp32] (signal)
 		0x41, 0x80, 0x3b, 0x00,                    					// +0x1c: cmp byte ptr [r11], 0
 		0x75, 0xfa,                              					// +0x20: jne 0x1c
 		0x48, 0xb8, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,	// +0x22: mov rax, imm64
 		0xff, 0xe0                               					// +0x2c: jmp rax
 	};
 
-	static constexpr size_t DH_OFF_OPCODE_DISP = 0x06;
-	static constexpr size_t DH_OFF_VMCS_DISP   = 0x10;
-	static constexpr size_t DH_OFF_SIGNAL_DISP = 0x1A;
-	static constexpr size_t DH_OFF_TRAMP_IMM64 = 0x2A;
+	static constexpr size_t DH_OFF_OPCODE_DISP = 0x04;
+	static constexpr size_t DH_OFF_VMCS_DISP   = 0x0e;
+	static constexpr size_t DH_OFF_SIGNAL_DISP = 0x18;
+	static constexpr size_t DH_OFF_TRAMP_IMM64 = 0x24;
 
 	bool install_decoder_hook(win_process* proc, vm_channel* channel) {
 		printf("[INF] Installing decoder hook.\n");
 
-		size_t stub_size = sizeof(decoder_hook);
-		uint8_t buffer[stub_size];
-
-		memcpy(buffer, decoder_hook, sizeof(decoder_hook));
 
 		uintptr_t sig_offset = superv::scanner::signature_scan(proc->handle, proc->address, proc->size, decoder_sig, decoder_mask);
 		if (!sig_offset) { 
 			printf("[ERR] signature_scan failed for decoder.\n"); 
-			return false; 
-		}
-
-		uintptr_t hook_addr = (uintptr_t)VirtualAllocEx(proc->handle, nullptr, stub_size, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
-		if (!hook_addr) { 
-			printf("[ERR] install_decoder_hook failed to allocate a hook: 0x%lx.\n", GetLastError()); 
 			return false; 
 		}
 
@@ -238,33 +228,47 @@ defer:
 			return false;
 		}
 
-		uintptr_t original_call = call_site + 5 + original_rel;
+		uintptr_t callee = call_site + 5 + original_rel;
+		uintptr_t trampoline = 0;
+		size_t n_prolg = 12;
 
+		if (!install_trampoline(proc->handle, callee, n_prolg, &trampoline)) {
+			printf("[ERR] install_decoder_hook::install_trampoline failed: 0x%lx.\n", GetLastError()); 
+			return false;
+		}
+
+		uintptr_t hook = (uintptr_t)VirtualAllocEx(proc->handle, nullptr, stub_size, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+		if (!hook) { 
+			printf("[ERR] install_decoder_hook failed to allocate a hook: 0x%lx.\n", GetLastError()); 
+			return false; 
+		}
+
+		size_t stub_size = sizeof(decoder_hook);
+		uint8_t buffer[stub_size];
+
+		memcpy(buffer, decoder_hook, stub_size);
 		uintptr_t ch_vmcs   = channel->self + offsetof(vm_channel, vmcs);
 		uintptr_t ch_opcode = channel->self + offsetof(vm_channel, ipc.opcode);
 		uintptr_t ch_signal = channel->self + offsetof(vm_channel, ipc.signal);
 		{
-			int32_t disp32_1 = (int32_t)(ch_opcode - (hook_addr + 0x0a));
-			memcpy(&buffer[DH_OFFSET_OPCODE_DISP], &disp32_1, sizeof(disp32_1));
+			int32_t d32_opcode = (int32_t)(ch_opcode - (hook + 0x08));
+			int32_t d32_vmcs = (int32_t)(ch_vmcs - (hook + 0x12));
+			int32_t d32_signal = (int32_t)(ch_signal - (hook + 0x1c));
 
-			int32_t disp32_2 = (int32_t)(ch_vmcs - (hook_addr + 0x14));
-			memcpy(&buffer[DH_OFF_VMCS_DISP], &disp32_2, sizeof(disp32_2));
-
-			int32_t disp32_3 = (int32_t)(ch_signal - (hook_addr + 0x1e));
-			memcpy(&buffer[DH_OFFSET_SIGNAL_DISP], &disp32_3, sizeof(disp32_3));
-
-			uint64_t abs_target = (uint64_t)original_call;
-			memcpy(&buffer[DH_OFFSET_TRAMP_IMM64], &abs_target, sizeof(abs_target)); 
+			memcpy(&buffer[DH_OFFSET_OPCODE_DISP], &d32_opcode, sizeof(int32_t));
+			memcpy(&buffer[DH_OFF_VMCS_DISP], &d32_vmcs, sizeof(int32_t));
+			memcpy(&buffer[DH_OFFSET_SIGNAL_DISP], &d32_signal, sizeof(int32_t));
+			memcpy(&buffer[DH_OFFSET_TRAMP_IMM64], &trampoline, sizeof(uintptr_t)); 
 		}
 
-		if (!rvm64::memory::write_process_memory(proc->handle, hook_addr, buffer, sizeof(decoder_hook))) {
+		if (!rvm64::memory::write_process_memory(proc->handle, hook, buffer, sizeof(decoder_hook))) {
 			printf("[ERR] install_decoder_hook::write_proc_memory failed to write the hook: 0x%lx.\n", GetLastError()); 
 			return false;
 		}
 
-		int32_t hook_rel = (int32_t)(hook_addr - (call_site + 5));
-		if (!rvm64::memory::write_process_memory(proc->handle, call_site + 1, (uint8_t*)&hook_rel, sizeof(hook_rel))) {
-			printf("[ERR] install_decoder_hook::write_proc_memory failed to patch decoder: 0x%lx.\n", GetLastError()); 
+		FlushInstructionCache(proc->handle, hook, stub_size);
+		if (!detour_callee(proc->handle, callee, hook, n_prolg)) {
+			printf("[ERR] install_decoder_hook::detour_callee failed to patch prologue: 0x%lx.\n", GetLastError()); 
 			return false;
 		}
 
