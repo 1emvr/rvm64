a0f6af65605e281101635bb019b0cef695b0403f
1emvr <lemur.proxymslns@outlook.com>
2025-08-11 03:40:57 -0400

    changing model for the vm/superv ipc

--- superv/main.cpp
+++ superv/main.cpp
@@ -21,7 +21,7 @@ namespace superv {
 			return 1;
 		}
 
-		vm_channel* channel = superv::loader::get_vm_channel(proc);
+		vm_channel* channel = superv::loader::vm_get_channel(proc);
 		if (!channel) {
 			printf("[ERR] Could not load vm channel\n");
 			return 1;
