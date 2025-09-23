3664cfe99a15ca869c5b4829a40fffc785b2a433
1emvr <lemur.proxymslns@outlook.com>
2025-09-23 04:59:50 -0400

    updated patches working. fix other unknown bugs.

--- superv/supr_utils.hpp
+++ superv/supr_utils.hpp
@@ -4,6 +4,7 @@
 namespace superv::utils {
 	void print_bytes(uint8_t *buffer, size_t size) {
 		printf("\n\t");
+
 		for (int i = 0; i < size; i++) {
 			std::printf("0x%02" PRIxPTR "%s", (uint8_t)buffer[i], ((i + 1) % 8 == 0) ? "\n\t" : "  ");
 		}
