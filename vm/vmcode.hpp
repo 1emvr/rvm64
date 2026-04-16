e9d9ee076e8903cca1357bdeb36acb0454d14775
1emvr <lemur.proxymslns@outlook.com>
2026-04-16 03:00:14 -0400

    refactoring

--- vm/vmcode.hpp
+++ vm/vmcode.hpp
@@ -51,6 +51,7 @@ DATA_SCN OPCODE EncodingTable [] = {
 };
 
 
+// TODO: Do not decrypt the table indicies directly.. instead find a way to copy them and decrypt locally.
 VMCALL VOID Decode (_In_ const UINT32 Opcode) {
 	UINT8 Decoded = 0;
 	UINT8 Opcode7 = Opcode & 0x7F;
