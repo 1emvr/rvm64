863bf1b7bef4e6dc5a018820a4a0a723ad116ce7
1emvr <lemur.proxymslns@outlook.com>
2026-04-16 03:00:14 -0400

    refactoring

--- superv/main.cpp
+++ superv/main.cpp
@@ -2,72 +2,63 @@
 #include <stdio.h>
 #include <stdint.h>
 
-//#include "supr_debug.hpp"
 #include "supr_load.hpp"
-//#include "supr_patch.hpp"
 #include "supr_scan.hpp"
 
 #include "../include/vmmain.hpp"
 #include "../include/vmproc.hpp"
 
-namespace superv {
-	void print_usage() {
-		printf("usage: superv --process <vm> --elf <riscv_elf>\n");
-	}
 
-	int64_t superv_main(const char* proc_name, const char* elf_name) {
-		win_process *proc = rvm64::process::get_process_info(proc_name);
-		if (!proc) {
-			printf("[ERR] could not find process information for target\n");
-			return 1;
-		}
-
-		vmcs_t* vmcs = superv::loader::get_vmcs(proc);
-		if (!vmcs) {
-			printf("[ERR] could not load vmcs\n");
-			return 1;
-		}
-
-		// if (!superv::patch::install_entry_hook(proc, channel)) {
-		// 	printf("[ERR] could not install entrypoint hook in the vm\n");
-		// 	return 1;
-		// }
-
-		// if (!superv::patch::install_decoder_hook(proc, channel)) {
-		// 	printf("[ERR] could not install decoder hook in the vm\n");
-		// 	return 1;
-		// }
-
-		if (!superv::loader::remote_write_file(proc->handle, vmcs, elf_name)) {
-			printf("[ERR] could not load elf to the vmcs\n");
-			return 1;
-		}
-
-		// return superv::debug::user_loop(proc, channel);
-		return 0; // forgetting about the debugger
-	}
+VOID PrintUsage () {
+	printf ("usage: superv --process <vm> --elf <riscv_elf>\n");
 }
 
+
+INT64 SupervMain (
+		_In_ const char* ProcName, 
+		_In_ const char* ElfName) 
+{
+	WIN_PROCESS *Proc = GetProcessInfo (ProcName);
+	if (!Proc) {
+		printf("[ERR] could not find process information for target\n");
+		return 1;
+	}
+
+	VMCS *Vmcs = GetVmcs (Proc);
+	if (!Vmcs) {
+		printf("[ERR] could not load vmcs\n");
+		return 1;
+	}
+
+	if (! RemoteWriteFile (Proc->Handle, Vmcs, ElfName)) {
+		printf("[ERR] could not load elf to the vmcs\n");
+		return 1;
+	}
+
+	return 0; 
+}
+
+
 int main(int argc, char** argv) {
-		const char *proc_name = nullptr;
-		const char *elf_name = nullptr;
+		const char *ProcName = nullptr;
+		const char *ElfName = nullptr;
 
 		if (argc < 5) {
-			superv::print_usage();
+			PrintUsage ();
 			return 1;
 		}
 		for (int i = 0; i < argc; i++) {
-			if (strcmp(argv[i], "-p") == 0 || strcmp(argv[i], "--process") == 0) {
-				proc_name = argv[i + 1];
+			if (strcmp (argv [i], "-p") == 0 || strcmp (argv [i], "--process") == 0) {
+				ProcName = argv[i + 1];
 			}
-			if (strcmp(argv[i], "-e") == 0 || strcmp(argv[i], "--elf") == 0) {
-				elf_name = argv[i + 1];
+			if (strcmp (argv [i], "-e") == 0 || strcmp (argv [i], "--elf") == 0) {
+				ElfName = argv [i + 1];
 			}
 		}
-		if (!proc_name || !elf_name) {
-			superv::print_usage();
+		if (! ProcName || ! ElfName) {
+			PrintUsage ();
 			return 1;
 		}
 
-		return superv::superv_main(proc_name, elf_name);
+		return SupervMain (ProcName, ElfName);
 }
