5699a86dd342065138389248fac0e58c44d389ef
Lemur <103511211+1emvr@users.noreply.github.com>
2026-09-07 18:58:08 -0400

    Update vmmain.cpp

--- vm/vmmain.cpp
+++ vm/vmmain.cpp
@@ -1,6 +1,8 @@
 #include <windows.h>
 #include "vmmain.hpp"
 
+// just realized that " forever threads " will need to have their own memory. 
+// we can separate one-time runs from the forever threads. this way there is no deadlock
 
 #define MAX_VM_THREADS 5
 struct PACKET_SEG { 
