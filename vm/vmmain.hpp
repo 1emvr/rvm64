8c5ad57e6713c5e73301909b44c44836276a89b3
Lemur <103511211+1emvr@users.noreply.github.com>
2026-09-07 18:58:08 -0400

    Update vmmain.cpp

--- vm/vmmain.hpp
+++ vm/vmmain.hpp
@@ -439,15 +439,11 @@ VOID NATIVE_CALL rvm64_main (
 		UINT64 type = g_vmcs->t_type [i];
 
 		if (type == SINGLE EXEC) {
-			if (!start_thread (
-						&g_vmcs->s_thread [i], (LPTHREAD_START_ROUTINE)thread_main, (LPVOID)&g_vmcs->t_args [i])) 
-			{
+			if (!start_thread (&g_vmcs->s_thread [i], (LPTHREAD_START_ROUTINE)thread_main, (LPVOID)&g_vmcs->t_args [i])) {
 				a->count -= 1;
 			}
 		} else if (type == INFINITE_EXEC) {
-			if (!start_thread (
-						&g_vmcs->s_thread [i], (LPTHREAD_START_ROUTINE)thread_main, (LPVOID)&g_vmcs->t_args [i])) 
-			{ // how do we access these infinite threads?
+			if (!start_thread (&g_vmcs->s_thread [i], (LPTHREAD_START_ROUTINE)thread_main, (LPVOID)&g_vmcs->t_args [i])) { // how do we access these infinite threads?
 				a->count -= 1;
 			}
 		} else {
